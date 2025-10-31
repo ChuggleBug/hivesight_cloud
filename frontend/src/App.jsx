aecacfae6a92b0c51c963fdbcc701009906cab51
Jacob <jacobgutierrez752@gmail.com>
2025-10-30 23:58:22 -0700

    Add tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function MainHeader() {
+  return (
+    <p>Hi</p>
+
+  );
+
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
