39f641c43555f4e416735088c860a7ea38a06505
Jacob <jacobgutierrez752@gmail.com>
2025-11-11 18:30:42 -0800

    allow header to navigate to home page

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -3,8 +3,10 @@ import { FaRegUser } from "react-icons/fa";
 import LocalStorageManager from "../util/localStorageManager";
 import { useEffect } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AppHeader() {
+    const navigate = useNavigate();
     const [username, setUsername] = useState('');
 
     useEffect(() => {
@@ -12,18 +14,20 @@ export default function AppHeader() {
     }, [username])
 
     return (
-            <div className="p-4 bg-hvs-yellow flex justify-between w-full">
+        <div className="p-4 bg-hvs-yellow flex justify-between w-full">
+            <button onClick={() => navigate('/')}>
                 <p className="hvs-text">HiveSight</p>
+            </button>
 
-                {/* Greeting only appears if user is logged in*/}
-                { LocalStorageManager.isLoggedIn() ?
-                    <div className="flex items-center gap-2">
-                        <p className="hvs-text">Welcome, {username}</p>
-                        <FaRegUser color="var(--color-hvs-white)" size={20}/>  
-                    </div>
+            {/* Greeting only appears if user is logged in*/}
+            {LocalStorageManager.isLoggedIn() ?
+                <div className="flex items-center gap-2">
+                    <p className="hvs-text">Welcome, {username}</p>
+                    <FaRegUser color="var(--color-hvs-white)" size={20} />
+                </div>
                 :
-                    <></>
-                }
-            </div>
+                <></>
+            }
+        </div>
     );
 }
