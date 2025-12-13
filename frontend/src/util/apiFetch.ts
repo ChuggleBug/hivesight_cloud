945c954b76ce3f884c8e1cef7239f5f9a7da4b78
Jacob <jacobgutierrez752@gmail.com>
2025-12-12 17:56:52 -0800

    Add real cloud endpoint

--- frontend/src/util/apiFetch.ts
+++ frontend/src/util/apiFetch.ts
@@ -1,5 +1,6 @@
 
 // A wrapper for fetch which handles hostname
 export default function apiFetch(input: string | URL | globalThis.Request, init?: RequestInit) {
-    return fetch(`http://localhost:3000${input}`, init);
+    return fetch(`http://ec2-34-238-248-180.compute-1.amazonaws.com${input}`, init);
 }
+
